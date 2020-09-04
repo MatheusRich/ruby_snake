32ae7858592b7b3b510a8d7dfb194e41b7290c75
Matheus Richard <matheusrichardt@gmail.com>
2020-09-04 12:05:24 -0300

    Add basic game loop

--- lib/ruby_snake.rb
+++ lib/ruby_snake.rb
@@ -6,10 +6,34 @@ require_relative 'ruby_snake/version'
 
 module RubySnake
   class Game < RichEngine::Game
-    def on_create; end
+    FPS = 1.0 / 60
 
-    def on_update(_x, _y)
-      true
+    def on_create
+      @game_objects = [Snake.new]
+    end
+
+    def on_update(dt, key)
+      game_over = false
+
+      # @canvas.clear
+      @game_objects.each do |obj|
+        obj.update(dt, key)
+      end
+
+      @game_objects.each do |obj|
+        obj.draw(@canvas)
+      end
+
+      binding.irb if key == :n
+      game_over = key == :q
+
+      render
+
+      sleep [FPS - dt, 0].max
+
+      !game_over
     end
   end
 end
+
+RubySnake::Game.play
